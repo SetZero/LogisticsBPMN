18a762f9b3cf7585d9be002eacfd4a2778b47d27
Sebastian <19Sebastian95@gmx.de>
2021-01-10 02:30:22 +0100

    Added barcode generation

--- camundaCode/src/main/java/rocks/magical/camunda/CreateLabel.java
+++ camundaCode/src/main/java/rocks/magical/camunda/CreateLabel.java
@@ -1,24 +1,44 @@
 package rocks.magical.camunda;
 
-import com.itextpdf.text.Document;
+import com.itextpdf.text.*;
 import com.itextpdf.text.Image;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.BarcodePDF417;
 import com.itextpdf.text.pdf.PdfDocument;
 import com.itextpdf.text.pdf.PdfWriter;
+import connectjar.org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import rocks.magical.camunda.database.utils.PackageUtil;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 @Component
 public class CreateLabel implements JavaDelegate {
     @Autowired
     private PackageUtil packageUtil;
+    Path tempDirWithPrefix;
+
+    public CreateLabel() {
+        try {
+            this.tempDirWithPrefix = Files.createTempDirectory("img_");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
@@ -30,14 +50,30 @@ public class CreateLabel implements JavaDelegate {
 
         System.out.println("Called create Label: \n" + packageCenter + "\nprice: " + price);
 
+        String barcode = generateBarcode(routeId);
+        // execution.setVariable("barcode", barcode); <- todo: camunda only accepts up to 4000 chars
+        execution.setVariable("price", price);
+    }
+
+    private String generateBarcode(String routeId) throws BadElementException, IOException {
         BarcodePDF417 barcode = new BarcodePDF417();
         barcode.setErrorLevel(5);
-        barcode.setCodeColumns(30);
-        barcode.setCodeRows(5);
+        barcode.setCodeColumns(20);
+        barcode.setCodeRows(20);
         barcode.setOptions(BarcodePDF417.PDF417_FIXED_RECTANGLE);
         barcode.setText(routeId);
         Image barcodeImage = barcode.getImage();
 
-        System.out.println(Arrays.toString(barcodeImage.getRawData()));
+        java.awt.Image awtImage = barcode.createAwtImage(Color.BLACK, Color.WHITE);
+        BufferedImage bImage= new BufferedImage(awtImage.getWidth(null), awtImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bImage.createGraphics();
+        g.drawImage(awtImage, 0, 0, null);
+        g.dispose();
+
+        File barcodeFile = File.createTempFile("barcode_", ".tmp", tempDirWithPrefix.toFile());
+        ImageIO.write(bImage, "png", barcodeFile);
+
+        byte[] encoded = Base64.encodeBase64(FileUtils.readFileToByteArray(barcodeFile));
+        return new String(encoded, StandardCharsets.US_ASCII);
     }
 }
--- camundaCode/src/test/java/rocks/magical/camunda/ProcessUnitTest.java
+++ camundaCode/src/test/java/rocks/magical/camunda/ProcessUnitTest.java
@@ -1,5 +1,9 @@
 package rocks.magical.camunda;
 
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.BarcodePDF417;
+import connectjar.org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 import org.apache.ibatis.logging.LogFactory;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
@@ -8,6 +12,15 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.init;
 
@@ -48,4 +61,32 @@ public class ProcessUnitTest {
 	  // Now: Drive the process by API and assert correct behavior by rocks.magical.camunda-bpm-assert
   }
 
+  @Test
+  public void testBarcodeGenerator() {
+    try {
+      Path tempDirWithPrefix = Files.createTempDirectory("img_");
+      BarcodePDF417 barcode = new BarcodePDF417();
+      barcode.setErrorLevel(5);
+      barcode.setCodeColumns(20);
+      barcode.setCodeRows(20);
+      barcode.setOptions(BarcodePDF417.PDF417_FIXED_RECTANGLE);
+      barcode.setText("ABC");
+      Image barcodeImage = barcode.getImage();
+
+      java.awt.Image awtImage = barcode.createAwtImage(Color.BLACK, Color.WHITE);
+      BufferedImage bImage= new BufferedImage(awtImage.getWidth(null), awtImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+      Graphics2D g = bImage.createGraphics();
+      g.drawImage(awtImage, 0, 0, null);
+      g.dispose();
+
+      File barcodeFile = File.createTempFile("barcode_", ".tmp", tempDirWithPrefix.toFile());
+      ImageIO.write(bImage, "png", barcodeFile);
+
+      byte[] encoded = Base64.encodeBase64(FileUtils.readFileToByteArray(barcodeFile));
+      String base64 = new String(encoded, StandardCharsets.US_ASCII);
+      System.out.println(base64);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
