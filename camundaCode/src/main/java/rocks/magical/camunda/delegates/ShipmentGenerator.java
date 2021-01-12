7215c377207174c0d39cb920030314d7f4b2693a
Sebastian <19Sebastian95@gmx.de>
2021-01-12 22:36:13 +0100

    fix process

--- camundaCode/src/main/java/rocks/magical/camunda/delegates/ShipmentGenerator.java
+++ camundaCode/src/main/java/rocks/magical/camunda/delegates/ShipmentGenerator.java
@@ -2,10 +2,29 @@ package rocks.magical.camunda.delegates;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import rocks.magical.camunda.database.entities.Driver;
+import rocks.magical.camunda.database.entities.PackageCenter;
+import rocks.magical.camunda.database.entities.Vehicle;
+import rocks.magical.camunda.database.utils.PackageUtil;
 
 public class ShipmentGenerator implements JavaDelegate {
+
+    @Autowired
+    private PackageUtil packageUtil;
+
     @Override
-    public void execute(DelegateExecution execution) throws Exception {
-        System.out.println("Called!");
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        String startLocation = delegateExecution.getVariable("location").toString();
+
+        PackageCenter packageCenter = packageUtil.getNearestPackageCenter(startLocation);
+        Driver driver = packageUtil.getBestDriverForPackageCenter(packageCenter);
+        Vehicle vehicle = packageUtil.getVehicleForDriver(driver);
+        Integer routeId = packageUtil.createRouteCandidate(startLocation, packageCenter, driver, vehicle);
+
+        delegateExecution.setVariable("routeId", routeId);
+        delegateExecution.setVariable("packageCenter", packageCenter);
+        delegateExecution.setVariable("driver", driver);
+        delegateExecution.setVariable("vehicle", vehicle);
     }
 }
