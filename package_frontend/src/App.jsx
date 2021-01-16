8993a4a3621c6a168ac9cc525c2b127fcacda001
Sebastian <19Sebastian95@gmx.de>
2021-01-16 01:46:00 +0100

    style ui

--- package_frontend/src/App.jsx
+++ package_frontend/src/App.jsx
@@ -1,11 +1,81 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import DeleteIcon from '@material-ui/icons/Delete';
+import GpsFixedIcon from '@material-ui/icons/GpsFixed';
+import UpdateIcon from '@material-ui/icons/Update';
+import TablePagination from '@material-ui/core/TablePagination';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import TableRow from '@material-ui/core/TableRow';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import AppBar from '@material-ui/core/AppBar';
+import Slide from '@material-ui/core/Slide';
+import Toolbar from '@material-ui/core/Toolbar';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import WidgetsIcon from '@material-ui/icons/Widgets';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
+import ScannerIcon from '@material-ui/icons/Scanner';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import HowToRegIcon from '@material-ui/icons/HowToReg';
+import ErrorIcon from '@material-ui/icons/Error';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
 import './App.css';
-import { locateMe, getGeoCoding, getReverseGeoCoding } from './utils/geoLocate'
+import { locateMe, getGeoCoding, getReverseGeoCoding } from './utils/geoLocate';
 
 const workerId = "aWorker";
 let currentId = "";
-//http://localhost:8080/engine-rest/engine/default/process-definition/key/PaketProzess/start
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+function getIconForPackageState(state) {
+  switch (state) {
+    case 'INFO_RECEIVED':
+      return (<Tooltip title="Elektronische Ankündigung erhalten"><LabelImportantIcon /></Tooltip>);
+    case 'COLLECTION_REQUEST':
+      return (<Tooltip title="Abholungsanfrage erhalten"><WidgetsIcon /></Tooltip>);
+    case 'PICK_UP':
+      return (<Tooltip title="Paket vom Ursprungsort aufgesammelt"><EmojiPeopleIcon /></Tooltip>);
+    case 'IN_TRANSIT':
+      return (<Tooltip title="Paket in Transit"><CompareArrowsIcon /></Tooltip>);
+    case 'HUB_SCAN':
+      return (<Tooltip title="Paket in Verteilerzentrum aufgesammelt"><ScannerIcon /></Tooltip>);
+    case 'OUT_FOR_DELIVERY':
+      return (<Tooltip title="In Zustellung"><LocalShippingIcon /></Tooltip>);
+    case 'DELIVERED':
+      return (<Tooltip title="Zugestellt"><HowToRegIcon /></Tooltip>);
+    default:
+      return (<Tooltip title="Unbekannt"><ErrorIcon /></Tooltip>);
+  }
+}
 
 async function startProcess() {
   let myHeaders = new Headers();
@@ -113,6 +183,28 @@ async function completeConfirmShipment(taskId, shipmentId, pAPIKey) {
   return completeTaskHelper(taskId, raw);
 }
 
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  card_root: {
+    minWidth: 275,
+  },
+  container: {
+    maxHeight: 840,
+  },
+  title: {
+    fontSize: 16,
+  },
+  pos: {
+    marginBottom: 12,
+    fontSize: 12,
+  },
+  barcode: {
+    maxWidth: 320
+  }
+});
+
 function App() {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
@@ -121,6 +213,7 @@ function App() {
   const [location, setLocation] = useState("");
   const [targetLocation, setTargetLocation] = useState("11.070280570708459 49.41817197216562");
   const [apiKey, setApiKey] = useState("HACKME");
+  const classes = useStyles();
 
   const [myShipments, setMyShipments] = useState([]);
 
@@ -164,108 +257,182 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        Paketverwaltung
-      </header>
-      <div>
-        <div>
-          <h2>Zusammenfassung</h2>
-          <div>Volumen: {width * height * depth} m³</div>
-          <div>Gewicht: {weight} m³</div>
-          <div><strong>Geschätzte Kosten: </strong> {Math.max(width * height * depth / 1000, weight)} € </div>
-        </div>
-        <form onSubmit={event => handleCompleteTask(event)}>
+      <HideOnScroll>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6">Paketverwaltung</Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Box m={10} />
+      <Box>
+        <Box mt={5}>
+          <Container>
+            <Card className={classes.card_root} variant="outlined">
+              <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                  Zusammenfassung
+            </Typography>
+                <Typography variant="h5" component="h2">
+                  {Math.max(width * height * depth / 1000, weight)} € <sup>*</sup>
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  *Geschätzte Kosten
+            </Typography>
+                <Typography variant="body2" component="p">
+                  <div>Volumen: {width * height * depth} cm³</div>
+                  <div>Gewicht: {weight} kg</div>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Container>
+        </Box>
 
-          <div>
-            <label>
-              Größe:
-              <input type="number" value={width} onChange={(event) => handleChangeInt(setWidth, event)} placeholder="Weite" /> <span> x </span>
-              <input type="number" value={height} onChange={(event) => handleChangeInt(setHeight, event)} placeholder="Höhe" /> <span> x </span>
-              <input type="number" value={depth} onChange={(event) => handleChangeInt(setDepth, event)} placeholder="Tiefe" />
-              <span> Meter</span>
-            </label>
-          </div>
+        <Box mt={5}>
+          <Container>
+            <Paper className={classes.root}>
+              <form onSubmit={event => handleCompleteTask(event)}>
+                <Container component="main">
+                  <Grid container spacing={2}>
+                    <Grid item xs={6} sm={4}>
+                      <TextField type="number"
+                        value={width}
+                        onChange={(event) => handleChangeInt(setWidth, event)}
+                        label="Weite"
+                        variant="outlined"
+                        fullWidth
+                        InputProps={{
+                          endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+                        }} />
+                    </Grid><Grid item xs={6} sm={4}>
+                      <TextField type="number"
+                        value={height}
+                        onChange={(event) => handleChangeInt(setHeight, event)}
+                        label="Höhe"
+                        variant="outlined"
+                        fullWidth
+                        InputProps={{
+                          endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+                        }} />
+                    </Grid><Grid item xs={12} sm={4}>
+                      <TextField type="number"
+                        value={depth}
+                        onChange={(event) => handleChangeInt(setDepth, event)}
+                        label="Tiefe"
+                        variant="outlined"
+                        fullWidth
+                        InputProps={{
+                          endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+                        }} />
+                    </Grid>
+                  </Grid>
+                  <Box m={2} />
+                  <Grid item xs={12} sm={12} mt={10}>
+                    <TextField
+                      label="Gewicht"
+                      value={weight} onChange={(event) => handleChangeInt(setWeight, event)}
+                      InputProps={{
+                        endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+                      }}
+                      variant="outlined"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Box m={2} />
+                  <Grid item xs={12} sm={12} mt={10}>
+                    <TextField
+                      label="Abholungsort"
+                      value={location} onChange={(event) => handleChange(setLocation, event)}
+                      InputProps={{
+                        endAdornment: <Button type="button" onClick={() => locateMe(setLocation)}><GpsFixedIcon /></Button>,
+                      }}
+                      variant="outlined"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Box m={2} />
+                  <Grid item xs={12} sm={12} mt={10}>
+                    <TextField
+                      label="Lieferort"
+                      value={targetLocation} onChange={(event) => handleChange(setTargetLocation, event)}
+                      variant="outlined"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Box m={2} />
+                  <Grid item xs={12} sm={12} mt={10}>
+                    <TextField
+                      label="API Key"
+                      value={apiKey} onChange={(event) => handleChange(setApiKey, event)}
+                      variant="outlined"
+                      type="password"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Box m={2} />
+                  <Button variant="contained" color="primary" type="submit">
+                    Sendung erstellen
+                </Button>
+                  <Box m={2} />
+                </Container>
+              </form>
+            </Paper>
+          </Container>
+        </Box>
 
-          <div>
-            <label>
-              Gewicht:
-              <input type="text" value={weight} onChange={(event) => handleChangeInt(setWeight, event)} placeholder="Gewicht" />
-              <span> kg</span>
-            </label>
-          </div>
+        <Box mt={5}>
+          <Container>
+            <Button onClick={() => getMyShipments(apiKey.valueOf())
+              .then((e) => {
+                e.json().then((j) => setMyShipments(j))
+              })
+            }><UpdateIcon /> Sendungen Aktialisieren</Button>
 
-          <div>
-            <label>
-              Abholungsort:
-              <input type="text" value={location} onChange={(event) => handleChange(setLocation, event)} placeholder="Ort" readOnly />
-              <button type="button" onClick={() => locateMe(setLocation)}>Lokalisieren</button>
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Lieferort:
-              <input type="text" value={targetLocation} onChange={(event) => handleChange(setTargetLocation, event)} placeholder="Ort" readOnly />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              API Key:
-              <input type="password" value={apiKey} onChange={(event) => handleChange(setApiKey, event)} placeholder="Ort" />
-            </label>
-          </div>
-          <input type="submit" value="Abschicken" />
-        </form>
-        <div>
-          <button onClick={() => getMyShipments(apiKey.valueOf())
-            .then((e) => {
-              e.json().then((j) => setMyShipments(j))
-            })
-          }>Get Shipments!</button>
-
-          <table>
-            <thead>
-              <tr>
-                <th>Customer Id</th>
-                <th>Shipment Id</th>
-                <th>Preis</th>
-                <th>Destination</th>
-                <th>Start Location</th>
-                <th>Attached Instance Id</th>
-                <th>Barcode</th>
-                <th>Zustand</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                myShipments.map((e, i) => {
-                  return (<tr key={i}>
-                    <td>{e.customerId}</td>
-                    <td>{e.shipmentId}</td>
-                    <td>{e.price ?? '---'} €</td>
-                    <td>{e.destination}</td>
-                    <td>{e.startLocation}</td>
-                    <td>{e.attachedProcessInstance}</td>
-                    <td>{e.barcode ? (<img src={"data:image/png;base64," + e.barcode} alt="barcode" />) : "---"}</td>
-                    <td>{e.state}</td>
-                    <td>
-                      {e.barcode ?
-                        e.state === 'INFO_RECEIVED' ?
-                        (<div>
-                          <button>Cancel</button>
-                          <button onClick={() => confirmShipment(e.shipmentId, e.attachedProcessInstance)}>Confirm</button>
-                        </div>) : "Verschickt" : "Waiting for manual confirmation"
-                      }
-                    </td>
-                  </tr>)
-                })
-              }
-            </tbody>
-          </table>
-        </div>
-      </div>
+            <Paper className={classes.root}>
+              <TableContainer className={classes.container}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Customer Id</TableCell>
+                      <TableCell>Shipment Id</TableCell>
+                      <TableCell>Preis</TableCell>
+                      <TableCell>Destination</TableCell>
+                      <TableCell>Start Location</TableCell>
+                      <TableCell>Barcode</TableCell>
+                      <TableCell>Zustand</TableCell>
+                      <TableCell>Aktion</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {
+                      myShipments.map((e, i) => {
+                        return (<TableRow hover role="checkbox" tabIndex={-1} key={i}>
+                          <TableCell>{e.customerId}</TableCell>
+                          <TableCell>{e.shipmentId}</TableCell>
+                          <TableCell>{e.price ?? '---'} €</TableCell>
+                          <TableCell>{e.destination}</TableCell>
+                          <TableCell>{e.startLocation}</TableCell>
+                          <TableCell>{e.barcode ? (<img className={classes.barcode} src={"data:image/png;base64," + e.barcode} alt="barcode" />) : "---"}</TableCell>
+                          <TableCell>{getIconForPackageState(e.state)}</TableCell>
+                          <TableCell>
+                            {e.barcode ?
+                              e.state === 'INFO_RECEIVED' ?
+                                (<ButtonGroup size="small" aria-label="small outlined button group">
+                                  <Button variant="contained" color="primary" onClick={() => confirmShipment(e.shipmentId, e.attachedProcessInstance)}>Bestätigen</Button>
+                                  <Button variant="outlined" color="secondary"><DeleteIcon /></Button>
+                                </ButtonGroup>) : (<Button disabled variant="contained" fullWidth><RemoveCircleIcon /></Button>) : "Waiting for manual confirmation"
+                            }
+                          </TableCell>
+                        </TableRow>)
+                      })
+                    }
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Paper>
+          </Container>
+        </Box>
+      </Box>
     </div >
   );
 }
